bdf2805e9790bce301b7217a234c7ffc7ec8185a
rahulchoudhary2961 <rahulchoudhary2961@gmail.com>
2022-12-02 04:21:42 +0530

    project added

---
+++ src/pages/homepage/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Directory from "../../components/menu-item/directory/Directory";
+
+import './Homepage.scss'
+
+const Homepage = () => {
+    return (
+        <div className='homepage'>
+            <Directory />
+        </div>
+    )
+}
+export default Homepage;
